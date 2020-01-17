b500b950fefcf1090f92fc53d3cc908ae4d9ab5b
Thanh <thanhly1245@gmail.com>
2020-01-16 20:44:54 -0800

    very basic hood

---
+++ src/main/java/competition/subsystems/hood/HoodSubsystem.java
@@ -0,0 +1,24 @@
+package competition.subsystems.hood;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import xbot.common.command.BaseSubsystem;
+import xbot.common.injection.wpi_factories.CommonLibFactory;
+
+@Singleton
+public class HoodSubsystem extends BaseSubsystem{
+
+    @Inject
+    public HoodSubsystem(CommonLibFactory factory){
+
+    }
+
+    public void setAngle(){
+
+    }
+
+    public void getAngle(){
+
+    }
+
+}
