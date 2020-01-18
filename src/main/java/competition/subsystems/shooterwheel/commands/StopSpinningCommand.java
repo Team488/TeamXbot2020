c477b7e800a4694d742c4a48963730730cdb9d53
jvdyfu <jvdyfu@gmail.com>
2020-01-18 15:49:18 -0800

    spin

--- src/main/java/competition/subsystems/shooterwheel/commands/StopSpinningCommand.java
+++ src/main/java/competition/subsystems/shooterwheel/commands/StopSpinningCommand.java
@@ -6,13 +6,13 @@ import competition.operator_interface.OperatorInterface;
 import competition.subsystems.shooterwheel.ShooterWheelSubsystem;
 import xbot.common.command.BaseCommand;
 
-public class SpinningShooterWheelCommand extends BaseCommand{    
+public class StopSpinningCommand extends BaseCommand{    
    
     final ShooterWheelSubsystem shooterWheelSubsystem;
     final OperatorInterface oi;
 
     @Inject
-    public SpinningShooterWheelCommand(OperatorInterface oi, ShooterWheelSubsystem shooterWheelSubsystem){
+    public StopSpinningCommand(OperatorInterface oi, ShooterWheelSubsystem shooterWheelSubsystem){
         this.oi = oi;
         this.shooterWheelSubsystem = shooterWheelSubsystem;
         this.requires(this.shooterWheelSubsystem);
@@ -24,6 +24,6 @@ public class SpinningShooterWheelCommand extends BaseCommand{
     }
 
     public void execute(){
-       shooterWheelSubsystem.spin();
+       shooterWheelSubsystem.stop();
     }
 }
