64183b0e804151f24b98117701f1271db990f33b
John Gilbert <johngilb@gmail.com>
2020-01-19 10:29:16 -0800

    Start work on electrical contract

---
+++ src/test/java/competition/BaseCompetitionTest.java
@@ -0,0 +1,37 @@
+package competition;
+
+import competition.operator_interface.OperatorInterface;
+import competition.subsystems.pose.PoseSubsystem;
+import xbot.common.injection.BaseWPITest;
+import xbot.common.injection.ElectricalContract;
+import xbot.common.injection.UnitTestModule;
+import xbot.common.subsystems.pose.BasePoseSubsystem;
+
+public class BaseCompetitionTest extends BaseWPITest {
+
+    protected OperatorInterface oi;
+    protected ElectricalContract contract;
+
+    protected class TestModule extends UnitTestModule {
+        @Override
+        protected void configure() {
+            super.configure();
+
+            this.bind(ElectricalContract.class).to(IdealElectricalContract.class);
+            this.bind(BasePoseSubsystem.class).to(PoseSubsystem.class);
+        }
+    }
+
+    public BaseCompetitionTest() {
+        guiceModule = new TestModule();
+    }
+
+    @Override
+    public void setUp() {
+        super.setUp();
+        
+        oi = injector.getInstance(OperatorInterface.class);
+        contract = injector.getInstance(ElectricalContract.class);
+    }
+    
+}
