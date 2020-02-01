416aa2facf2a922c5b28bea999f957e3cd5fca38
lil206chris <big206chris@gmail.com>
2020-02-01 10:55:48 -0800

    Electrical Contract

--- src/main/java/competition/IdealElectricalContract.java
+++ src/main/java/competition/IdealElectricalContract.java
@@ -8,7 +8,7 @@ public class IdealElectricalContract extends ElectricalContract {
     // Drive
     ///
     public DeviceInfo leftFrontDriveNeo() {
-        return new DeviceInfo(32, false);
+        return new DeviceInfo(32, true);
     }
 
     public DeviceInfo leftRearDriveNeo() {
@@ -23,10 +23,39 @@ public class IdealElectricalContract extends ElectricalContract {
         return new DeviceInfo(22, false);
     }
 
+    //Everything above sure about inversion
+    //Everything below not sure about the inversion
     public DeviceInfo intakeMotor()
     {
         return new DeviceInfo(24, false);
     }
 
+    public DeviceInfo shooterMotorMaster()
+    {
+        return new DeviceInfo(21, false);
+    }
+    public DeviceInfo shooterMotorFollower()
+    {
+        return new DeviceInfo(34, false);
+    }
+
+    public DeviceInfo climberMotorMaster()
+    {
+        return new DeviceInfo(35, false);
+    }
+    public DeviceInfo climberMotorfollower()
+    {
+        return new DeviceInfo(20, false);
+    }
+
+    public DeviceInfo rotationMotor()
+    {
+        return new DeviceInfo(31, false);
+    }
+
+    public DeviceInfo carouselMotor()
+    {
+        return new DeviceInfo(25, false);
+    }
 
 }
