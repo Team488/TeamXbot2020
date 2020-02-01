9c6c376d4239091997e87bf52bd6f254b79f5fa0
lil206chris <big206chris@gmail.com>
2020-02-01 13:27:22 -0800

    turn left/right commands and canturn in subsystem

--- src/main/java/competition/subsystems/turret/TurretSubsystem.java
+++ src/main/java/competition/subsystems/turret/TurretSubsystem.java
@@ -7,28 +7,56 @@ import competition.IdealElectricalContract;
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XCANTalon;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
+import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
 
 @Singleton
 public class TurretSubsystem extends BaseSubsystem {
     
     public XCANTalon motor;
+    DoubleProperty maxAngleProp;
+    DoubleProperty minAngleProp;
+    DoubleProperty turnSpeedProp;
+
     @Inject
     public TurretSubsystem(CommonLibFactory factory, PropertyFactory pf, IdealElectricalContract contract) {
         log.info("Creating TurretSubsystem");
         pf.setPrefix(this);
         
+        maxAngleProp = pf.createPersistentProperty("MaxAngle",180);
+        minAngleProp = pf.createPersistentProperty("MinAngle", -180);
+        turnSpeedProp = pf.createPersistentProperty("TurnSpeed", .03); 
+
+
         this.motor = factory.createCANTalon(contract.rotationMotor().channel);
         motor.setInverted(contract.rotationMotor().inverted);
     }
 
+    public void TurnLeft()
+    {
+        motor.simpleSet(-turnSpeedProp.get());
+    }
 
+    public void TurnRight()
+    {
+        motor.simpleSet(turnSpeedProp.get());
+    }
 
     public void setPower(double power) {
         motor.simpleSet(power);
     }
-  
-    public void stop () {
+    
+    public boolean canTurnRight(double currentAngle)
+    {
+        return currentAngle <= maxAngleProp.get()-turnSpeedProp.get();
+    }
+
+    public boolean canTurnLeft(double currentAngle)
+    {
+        return currentAngle >= minAngleProp.get()+turnSpeedProp.get();
+    }
+
+    public void stop() {
         setPower(0);
     }
 }
---
+++ src/main/java/competition/subsystems/turret/commands/TurnLeft.java
@@ -0,0 +1,35 @@
+package competition.subsystems.turret.commands;
+
+import com.google.inject.Inject;
+
+import competition.operator_interface.OperatorInterface;
+import competition.subsystems.turret.TurretSubsystem;
+import xbot.common.command.BaseCommand;
+
+public class TurnLeft extends BaseCommand
+{
+    final OperatorInterface oi;
+    final TurretSubsystem turretSubsystem;
+
+    @Inject
+    public TurnLeft(OperatorInterface oi , TurretSubsystem tSubsystem) {
+        this.oi = oi;
+        this.turretSubsystem = tSubsystem;
+
+        this.addRequirements(this.turretSubsystem);
+
+    }
+
+    @Override
+    public void execute() {
+        turretSubsystem.setPower(0);
+
+    }
+
+    @Override
+    public void initialize() {
+        log.info("Initializing");
+
+    }
+    
+}
