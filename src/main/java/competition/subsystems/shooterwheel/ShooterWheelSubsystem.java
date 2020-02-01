62a0f46f1b2b0fcfd75a4813a32ea1dfde389794
lil206chris <big206chris@gmail.com>
2020-02-01 12:39:48 -0800

    Implemented sparkmaxs in the subsystem

--- src/main/java/competition/subsystems/shooterwheel/ShooterWheelSubsystem.java
+++ src/main/java/competition/subsystems/shooterwheel/ShooterWheelSubsystem.java
@@ -3,6 +3,7 @@ package competition.subsystems.shooterwheel;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import competition.IdealElectricalContract;
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XCANSparkMax;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
@@ -18,10 +19,13 @@ public class ShooterWheelSubsystem extends BaseSubsystem {
     
     
     @Inject
-    public ShooterWheelSubsystem(CommonLibFactory factory, PropertyFactory pf) {
+    public ShooterWheelSubsystem(CommonLibFactory factory, PropertyFactory pf, IdealElectricalContract contract) {
         log.info("Creating ShooterWheelSubsystem");
         pf.setPrefix(this);
         spinWheelPowerProp = pf.createPersistentProperty("Spinning Wheel Power", 1);
+
+        this.neoMasterMotor = factory.createCANSparkMax(contract.shooterMotorMaster().channel, this.getPrefix(), "ShooterMaster");
+        this.neoFollowerMotor = factory.createCANSparkMax(contract.shooterMotorFollower().channel, this.getPrefix(), "ShooterFollower");
 
 
     }
