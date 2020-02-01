733dfa0eacaf802533f6a554e876a08047e27281
John Gilbert <johngilb@gmail.com>
2020-02-01 10:57:34 -0800

    User newer commonlib

--- src/main/java/competition/operator_interface/OperatorInterface.java
+++ src/main/java/competition/operator_interface/OperatorInterface.java
@@ -3,7 +3,6 @@ package competition.operator_interface;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
-import xbot.common.controls.sensors.XFTCGamepad;
 import xbot.common.controls.sensors.XXboxController;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
 import xbot.common.logging.RobotAssertionManager;
