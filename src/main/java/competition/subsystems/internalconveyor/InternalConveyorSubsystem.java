d9f77a9c870327bd9caebf56e8dcaf7d2f32ee2a
lil206chris <55634496+lil206chris@users.noreply.github.com>
2020-02-01 11:32:33 -0800

    Feature/electrical contract (#13)
    * intake motor added
    
    * Electrical Contract
    
    * Fixed electrical Contract

--- src/main/java/competition/subsystems/internalconveyor/InternalConveyorSubsystem.java
+++ src/main/java/competition/subsystems/internalconveyor/InternalConveyorSubsystem.java
@@ -2,7 +2,9 @@ package competition.subsystems.internalconveyor;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import competition.IdealElectricalContract;
 import xbot.common.command.BaseSubsystem;
+import xbot.common.controls.actuators.XCANTalon;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
@@ -13,13 +15,17 @@ public class InternalConveyorSubsystem extends BaseSubsystem { //makes conveyer 
 
     final DoubleProperty intakePowerProp;
     final DoubleProperty outtakePowerProp;
+    public final XCANTalon intakeMotor;
 
     @Inject
-    public InternalConveyorSubsystem(CommonLibFactory factory, PropertyFactory pf){
+    public InternalConveyorSubsystem(CommonLibFactory factory, PropertyFactory pf, IdealElectricalContract contract){
 
         pf.setPrefix(this);
         intakePowerProp = pf.createPersistentProperty("IntakePower", 0.5);
         outtakePowerProp = pf.createPersistentProperty("OuttakePower", 0.5);
+
+        this.intakeMotor = factory.createCANTalon(contract.intakeMotor().channel);
+        intakeMotor.setInverted(contract.intakeMotor().inverted);
     }
 
     public void intake(){
