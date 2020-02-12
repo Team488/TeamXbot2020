8e9805dbd3bef9ea549f2feb100ef44bbb7829fd
lil206chris <big206chris@gmail.com>
2020-02-11 19:28:43 -0800

    Turn Left and right tests

--- src/test/java/competition/subsystems/turret/TurretSubsystemTest.java
+++ src/test/java/competition/subsystems/turret/TurretSubsystemTest.java
@@ -9,8 +9,25 @@ import competition.BaseCompetitionTest;
 public class TurretSubsystemTest extends BaseCompetitionTest {
 
     @Test
+    public void testTurnRight() {
+        TurretSubsystem turret = this.injector.getInstance(TurretSubsystem.class);
+        turret.turnRight();
+
+        assertEquals(-0.03 , turret.motor.getMotorOutputPercent(), 0.001);
+    }
+
+    @Test
+    public void testTurnLeft() {
+        TurretSubsystem turret = this.injector.getInstance(TurretSubsystem.class);
+        turret.turnLeft();
+
+        assertEquals(0.03 , turret.motor.getMotorOutputPercent(), 0.001);
+    }
+
+    @Test
     public void testStop() {
         TurretSubsystem turret = this.injector.getInstance(TurretSubsystem.class);
+        turret.turnRight();
         turret.stop();
 
         assertEquals(0 , turret.motor.getMotorOutputPercent(), 0.001);
