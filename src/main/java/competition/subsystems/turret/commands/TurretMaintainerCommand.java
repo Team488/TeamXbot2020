2f2050a02c343daad483abb57baa6a9200ade112
lil206chris <big206chris@gmail.com>
2020-02-15 10:38:23 -0800

    added package

--- src/main/java/competition/subsystems/turret/commands/TurretMaintainerCommand.java
+++ src/main/java/competition/subsystems/turret/commands/TurretMaintainerCommand.java
@@ -1,21 +1,26 @@
+package competition.subsystems.turret.commands;
+
 import com.google.inject.Inject;
 
+import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.turret.TurretSubsystem;
 import xbot.common.command.BaseCommand;
 
 public class TurretMaintainerCommand extends BaseCommand {
 
     final TurretSubsystem turretSubsystem;
+    final DriveSubsystem driveSubsystem;
 
     @Inject
-    public TurretMaintainerCommand(TurretSubsystem tSubsystem) {
+    public TurretMaintainerCommand(TurretSubsystem tSubsystem, DriveSubsystem driveSubsystem) {
         this.turretSubsystem = tSubsystem;
+        this.driveSubsystem = driveSubsystem;
         this.addRequirements(this.turretSubsystem);
     }
 
     @Override
     public void execute() {
-        turret
+        
     }
 
     @Override
