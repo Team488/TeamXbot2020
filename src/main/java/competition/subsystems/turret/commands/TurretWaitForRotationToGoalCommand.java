66ad6caa4a96ee77ec324df53c2a1a58675fce20
Stephen Just <stephenjust@gmail.com>
2020-02-17 12:42:08 -0800

    Feature/Stub for command group to pass power cell downrange (#30)

---
+++ src/main/java/competition/subsystems/turret/commands/TurretWaitForRotationToGoalCommand.java
@@ -0,0 +1,49 @@
+package competition.subsystems.turret.commands;
+
+import com.google.inject.Inject;
+import competition.subsystems.turret.TurretSubsystem;
+import xbot.common.command.BaseCommand;
+import xbot.common.controls.sensors.XTimer;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.PropertyFactory;
+
+public class TurretWaitForRotationToGoalCommand extends BaseCommand {
+
+    private final TurretSubsystem turretSubsystem;
+    private final DoubleProperty timeoutProperty;
+    private final DoubleProperty errorProperty;
+
+    private double startTime = -1;
+
+    @Inject
+    public TurretWaitForRotationToGoalCommand(PropertyFactory pf, TurretSubsystem tSubsystem) {
+        this.turretSubsystem = tSubsystem;
+        this.addRequirements(this.turretSubsystem);
+
+        pf.setPrefix(this);
+
+        this.timeoutProperty = pf.createPersistentProperty("Timeout", 1);
+        this.errorProperty = pf.createPersistentProperty("Comparision threshold", 1.0);
+    }
+
+    @Override
+    public void initialize() {
+        log.info("Initializing");
+        this.startTime = XTimer.getFPGATimestamp();
+    }
+
+    @Override
+    public void execute() {
+        // This command doesn't do anything
+    }
+
+    @Override
+    public boolean isFinished() {
+        double currentAngle = this.turretSubsystem.getCurrentAngle();
+        double goalAngle = this.turretSubsystem.getGoalAngle();
+        
+        return Math.abs(currentAngle - goalAngle) < this.errorProperty.get()
+            || XTimer.getFPGATimestamp() - this.startTime > this.timeoutProperty.get();
+    }
+    
+}
---
+++ src/test/java/competition/subsystems/turret/TurretWaitForRotationToGoalCommandTest.java
@@ -0,0 +1,54 @@
+package competition.subsystems.turret;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import competition.BaseCompetitionTest;
+import competition.subsystems.turret.commands.TurretWaitForRotationToGoalCommand;
+import edu.wpi.first.wpilibj.MockTimer;
+import xbot.common.properties.PropertyFactory;
+
+public class TurretWaitForRotationToGoalCommandTest extends BaseCompetitionTest {
+
+    @Test
+    public void testAtGoal() {
+        TurretSubsystem turret = this.injector.getInstance(TurretSubsystem.class);
+        PropertyFactory pf = this.injector.getInstance(PropertyFactory.class);
+
+        TurretWaitForRotationToGoalCommand command = new TurretWaitForRotationToGoalCommand(pf, turret);
+
+        turret.setGoalAngle(turret.getCurrentAngle() + 90);
+
+        command.initialize();
+        command.execute();
+
+        assertFalse(command.isFinished());
+
+        turret.setGoalAngle(turret.getCurrentAngle());
+
+        assertTrue(command.isFinished());
+    }
+
+    @Test
+    public void testTimeout() {
+        TurretSubsystem turret = this.injector.getInstance(TurretSubsystem.class);
+        PropertyFactory pf = this.injector.getInstance(PropertyFactory.class);
+
+        TurretWaitForRotationToGoalCommand command = new TurretWaitForRotationToGoalCommand(pf, turret);
+
+        turret.setGoalAngle(turret.getCurrentAngle() + 90);
+
+        command.initialize();
+        command.execute();
+
+        assertFalse(command.isFinished());
+
+        MockTimer mockTimer = injector.getInstance(MockTimer.class);        
+        mockTimer.advanceTimeInSecondsBy(10);
+
+        assertTrue(command.isFinished());
+    }
+
+}
