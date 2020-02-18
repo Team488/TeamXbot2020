8c5d0d503ba220d6e22a0bb85c50bd24ce56f0fb
John Gilbert <johngilb@gmail.com>
2020-02-17 16:57:26 -0800

    Use new maintainer logicc

--- src/main/java/competition/subsystems/shooterwheel/commands/ShooterWheelMaintainerCommand.java
+++ src/main/java/competition/subsystems/shooterwheel/commands/ShooterWheelMaintainerCommand.java
@@ -4,15 +4,18 @@ import com.google.inject.Inject;
 
 import competition.operator_interface.OperatorInterface;
 import competition.subsystems.shooterwheel.ShooterWheelSubsystem;
-import xbot.common.command.BaseCommand;
+import xbot.common.command.BaseMaintainerCommand;
+import xbot.common.injection.wpi_factories.CommonLibFactory;
+import xbot.common.properties.PropertyFactory;
 
-public class SpinningShooterWheelCommand extends BaseCommand{
+public class ShooterWheelMaintainerCommand extends BaseMaintainerCommand {
    
     final ShooterWheelSubsystem wheel;
     final OperatorInterface oi;
 
     @Inject
-    public SpinningShooterWheelCommand(OperatorInterface oi, ShooterWheelSubsystem wheel){
+    public ShooterWheelMaintainerCommand(OperatorInterface oi, ShooterWheelSubsystem wheel, PropertyFactory pf, CommonLibFactory clf){
+        super(wheel, pf, clf, 50, 0.25);
         this.oi = oi;
         this.wheel = wheel;
         this.addRequirements(this.wheel);
@@ -25,7 +28,8 @@ public class SpinningShooterWheelCommand extends BaseCommand{
         wheel.configurePID();
     }
 
-    public void execute(){
+    @Override
+    protected void maintain() {
         double speed = wheel.getTargetRPM();
         wheel.setPidSetpoint(speed);
     }
@@ -33,5 +37,16 @@ public class SpinningShooterWheelCommand extends BaseCommand{
     @Override
     public void end(boolean interrupted) {
         wheel.resetWheel();
+    }
+
+    @Override
+    protected void calibratedMachineControlAction() {
+        double speed = wheel.getTargetRPM();
+        wheel.setPidSetpoint(speed);
+    }
+
+    @Override
+    protected double getHumanInput() {
+        return 0;
     }
 }
