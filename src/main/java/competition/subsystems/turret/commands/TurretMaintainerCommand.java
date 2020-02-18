fcdc6583b0b30263642ca83c317c0d25bbaba330
JohnGilb <johngilb@gmail.com>
2020-02-17 17:19:13 -0800

    Use new maintainer logic (#32)
    * Use new maintainer logicc
    
    * clean up

--- src/main/java/competition/subsystems/turret/commands/TurretMaintainerCommand.java
+++ src/main/java/competition/subsystems/turret/commands/TurretMaintainerCommand.java
@@ -4,16 +4,15 @@ import com.google.inject.Inject;
 
 import competition.operator_interface.OperatorInterface;
 import competition.subsystems.turret.TurretSubsystem;
-import xbot.common.command.BaseCommand;
+import xbot.common.command.BaseMaintainerCommand;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
 import xbot.common.logic.HumanVsMachineDecider;
-import xbot.common.logic.HumanVsMachineDecider.HumanVsMachineMode;
 import xbot.common.math.MathUtils;
 import xbot.common.math.PIDFactory;
 import xbot.common.math.PIDManager;
 import xbot.common.properties.PropertyFactory;
 
-public class TurretMaintainerCommand extends BaseCommand {
+public class TurretMaintainerCommand extends BaseMaintainerCommand {
 
     final TurretSubsystem turret;
     final PIDManager pid;
@@ -22,11 +21,11 @@ public class TurretMaintainerCommand extends BaseCommand {
 
     @Inject
     public TurretMaintainerCommand(TurretSubsystem turret, PropertyFactory pf, PIDFactory pidf, CommonLibFactory clf, OperatorInterface oi) {
+        super(turret, pf, clf, 1, 0.33);
         this.turret = turret;
         this.oi = oi;
         decider = clf.createHumanVsMachineDecider(this.getPrefix());
         pid = pidf.createPIDManager("TurretPID", 0.04, 0, 0);
-        this.addRequirements(turret);
     }
 
     @Override
@@ -35,35 +34,13 @@ public class TurretMaintainerCommand extends BaseCommand {
     }
 
     @Override
-    public void execute() {
-        double power = 0;
-        double humanInput = MathUtils.deadband(oi.operatorGamepad.getRightVector().x, oi.getJoystickDeadband());
-        HumanVsMachineMode mode = decider.getRecommendedMode(humanInput);
-
-        switch (mode) {
-            case Coast:
-                // Do nothing
-                break;
-            case HumanControl:
-                power = humanInput;
-                break;
-            case InitializeMachineControl:
-                turret.setGoalAngle(turret.getCurrentAngle());
-                break;
-            case MachineControl:
-                if (turret.isCalibrated()){
-                    power = pid.calculate(turret.getGoalAngle(), turret.getCurrentAngle());
-                } else {
-                    // TODO: Some kind of calibration routine. For now, direct human input.
-                    power = humanInput;
-                }
-                break;
-            default:
-                // How did you get here?!?!
-                break;
-    
-        }
-
+    protected void calibratedMachineControlAction() {
+        double power = pid.calculate(turret.getGoalAngle(), turret.getCurrentAngle());
         turret.setPower(power);
+    }
+
+    @Override
+    protected double getHumanInput() {
+        return MathUtils.deadband(oi.operatorGamepad.getRightVector().x, oi.getJoystickDeadband());
     }
 }
