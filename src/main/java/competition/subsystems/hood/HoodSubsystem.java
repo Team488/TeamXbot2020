3c59b94d109c8afc09f18f9839f650cf62a4e674
Thanh <thanhly1245@gmail.com>
2020-02-18 20:48:13 -0800

    edited hood subsystem for maintainer

--- src/main/java/competition/subsystems/hood/HoodSubsystem.java
+++ src/main/java/competition/subsystems/hood/HoodSubsystem.java
@@ -3,53 +3,83 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import competition.IdealElectricalContract;
 import xbot.common.command.BaseSetpointSubsystem;
-import xbot.common.command.BaseSubsystem;
+import xbot.common.command.XScheduler;
 import xbot.common.controls.actuators.XCANTalon;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
 import xbot.common.math.MathUtils;
-import xbot.common.math.PIDManager;
+import xbot.common.properties.BooleanProperty;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
 
 @Singleton
 public class HoodSubsystem extends BaseSetpointSubsystem{
 
+    private double calibrationOffset;
     final DoubleProperty extendPowerProp;
     final DoubleProperty retractPowerProp;
     final DoubleProperty maxAngleProp;
     final DoubleProperty minAngleProp;
+    final DoubleProperty defaultForwardHeadingProp;
+    final DoubleProperty ticksPerDegreeProp;
+    final BooleanProperty calibratedProp;
     final DoubleProperty currentAngleProp;
     private IdealElectricalContract contract;
-    double maxAngle;
-    double minAngle;
     public XCANTalon hoodMotor;
+    double goalAngle;
     double angle;
 
     @Inject
-    public HoodSubsystem (CommonLibFactory factory, PropertyFactory pf, IdealElectricalContract contract){
+    public HoodSubsystem (CommonLibFactory factory, PropertyFactory pf, IdealElectricalContract contract,
+    XScheduler scheduler){
+        log.info("Creating HoodSubsystem");
         pf.setPrefix(this);
         this.contract = contract;
+
+        calibrationOffset = 0;
         extendPowerProp = pf.createPersistentProperty("Extend Power", 1);
         retractPowerProp = pf.createPersistentProperty("Retract Power", -1);
         maxAngleProp = pf.createPersistentProperty("Max Angle", 5);
         minAngleProp = pf.createPersistentProperty("Min Angle", -5);
+        defaultForwardHeadingProp = pf.createPersistentProperty("Default Forward Heading", 0);
+        ticksPerDegreeProp = pf.createPersistentProperty("Ticks Per Degree", 1);
         currentAngleProp = pf.createEphemeralProperty("Current Angle", 0);
+        calibratedProp = pf.createEphemeralProperty("Calibrated", false);
 
         if (contract.isHoodReady()) {
              this.hoodMotor = factory.createCANTalon(contract.hoodMotor().channel);
         }
 
-        maxAngle = 5;
-        minAngle = -5;
-
+        scheduler.registerSubsystem(this);
     }
 
-    public double getAngle(){
-        return angle;
+    public void calibrateHood(){
+        calibrationOffset = getCurrentRawAngle();
+        log.info("Angle set to the default of" + defaultForwardHeadingProp.get());
+        setIsCalibrated(true);
+    }
+    
+    public void setIsCalibrated(boolean value){
+        calibratedProp.set(value);
     }
 
     public void setAngle(double angle){
-        this.angle = angle;
+        currentAngleProp.set(angle);
+    }
+
+    public void uncalibrate(){
+        setIsCalibrated(false);
+    }
+
+    public boolean isCalibrated(){
+        return calibratedProp.get();
+    }
+
+    public void setGoalAngle(double angle){
+        goalAngle = angle;
+    }
+
+    public double getGoalAngle(){
+        return goalAngle;
     }
 
     public void extend(){
@@ -65,19 +95,22 @@ public class HoodSubsystem extends BaseSetpointSubsystem{
     }
 
     public boolean isFullyExtended(){
-        return (getAngle() >= maxAngle);
+        return (getCurrentAngle() >= maxAngleProp.get());
     }
 
     public boolean isFullyRetracted(){
-        return (getAngle() <= minAngle);
+        return (getCurrentAngle() <= minAngleProp.get());
     }
 
     public void setPower(double power){
-        if(isFullyExtended()){
+        if(isCalibrated()){
+
+            if(isFullyExtended()){
             power = MathUtils.constrainDouble(power, -1, 0);
-        }
-        if(isFullyRetracted()){
+            }
+            if(isFullyRetracted()){
             power = MathUtils.constrainDouble(power, 0, 1);
+            }
         }
 
         if (contract.isHoodReady()) {
@@ -85,4 +118,34 @@ public class HoodSubsystem extends BaseSetpointSubsystem{
         }
     }
 
+    public void periodic(){
+        currentAngleProp.set(getCurrentAngle());
+    }
+
+    public double getCurrentAngle(){
+        double ticks = getCurrentRawAngle() - calibrationOffset;
+        return (ticks / ticksPerDegreeProp.get()) + defaultForwardHeadingProp.get();
+    }
+
+    public double getCurrentRawAngle(){
+        if(contract.isHoodReady()){
+            return hoodMotor.getSelectedSensorPosition(0);
+        }
+        return 0;
+    }
+
+    @Override
+    public double getCurrentValue(){
+        return getCurrentAngle();
+    }
+
+    @Override
+    public double getTargetValue(){
+        return getGoalAngle();
+    }
+
+    @Override
+    public void setTargetValue(double value){
+        setGoalAngle(value);
+    }
 }
---
+++ src/main/java/competition/subsystems/hood/commands/HoodMaintainerCommand.java
@@ -0,0 +1,47 @@
+package competition.subsystems.hood.commands;
+import com.google.inject.Inject;
+
+import competition.operator_interface.OperatorInterface;
+import competition.subsystems.hood.HoodSubsystem;
+import xbot.common.command.BaseMaintainerCommand;
+import xbot.common.injection.wpi_factories.CommonLibFactory;
+import xbot.common.logic.HumanVsMachineDecider;
+import xbot.common.math.MathUtils;
+import xbot.common.math.PIDFactory;
+import xbot.common.math.PIDManager;
+import xbot.common.properties.PropertyFactory;
+
+public class HoodMaintainerCommand extends BaseMaintainerCommand{
+
+    final HoodSubsystem hood;
+    final PIDManager pid;
+    final HumanVsMachineDecider decider;
+    final OperatorInterface oi;
+
+    @Inject
+    public HoodMaintainerCommand(HoodSubsystem hood, PropertyFactory pf, PIDFactory pidf, CommonLibFactory clf,
+    OperatorInterface oi){
+        super(hood, pf, clf, 1, 0.33);
+        this.hood = hood;
+        this.oi = oi;
+        decider = clf.createHumanVsMachineDecider(this.getPrefix());
+        pid = pidf.createPIDManager("HoodPID", 0.04, 0, 0);
+
+    }
+
+    @Override
+    public void initialize(){
+        log.info("Initializing");
+    }
+
+    @Override
+    protected void calibratedMachineControlAction(){
+        double power = pid.calculate(hood.getGoalAngle(), hood.getCurrentAngle());
+        hood.setPower(power);
+    }
+
+    @Override
+    protected double getHumanInput(){
+        return MathUtils.deadband(oi.operatorGamepad.getLeftVector().y, oi.getJoystickDeadband());
+    }
+}
