f28d3ecdcbd4102e95b89b615dcc65a374f14d6a
Stephen Just <stephenjust@gmail.com>
2020-02-19 08:53:09 -0800

    Feature/Enhance pass command to spin the shooter wheel (#34)
    * Bug/Point turret command must complete
    
    Unless we set isFinished() on the rotation command, we will never proceed to the next command in the group.
    
    Fix command map to only trigger shooting once when the button is pressed
    
    * Revert unintended change
    
    * Review comments
    
    * Use the turret maintainer's "at goal"
    
    * Pass towards target
    
    * Test for wait command

--- src/main/java/competition/commandgroups/PassTowardsTargetCommand.java
+++ src/main/java/competition/commandgroups/PassTowardsTargetCommand.java
@@ -2,23 +2,34 @@ package competition.commandgroups;
 
 import com.google.inject.Inject;
 
+import competition.subsystems.shooterwheel.commands.ShooterWheelSetPassPowerCommand;
+import competition.subsystems.shooterwheel.commands.ShooterWheelWaitForGoalCommand;
 import competition.subsystems.turret.commands.PointTurretToFieldOrientedHeadingCommand;
 import competition.subsystems.turret.commands.TurretWaitForRotationToGoalCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class PassTowardsTargetCommand extends SequentialCommandGroup {
+public class PassTowardsTargetCommand extends ParallelCommandGroup {
 
     @Inject
     public PassTowardsTargetCommand(PointTurretToFieldOrientedHeadingCommand rotateTurretCommand,
-        TurretWaitForRotationToGoalCommand waitForRotateTurretCommand) {
+        ShooterWheelSetPassPowerCommand shooterWheelPowerCommand,
+        TurretWaitForRotationToGoalCommand waitForRotateTurretCommand,
+        ShooterWheelWaitForGoalCommand waitForShooterWheelSpeedCommand) {
 
         this.addCommands(
             rotateTurretCommand,
-            waitForRotateTurretCommand
-            // shooter wheel
-            // adjust hood
-            // kicker
+            shooterWheelPowerCommand,
             // carousel
+            // hood
+            new SequentialCommandGroup(
+                new ParallelCommandGroup(
+                    waitForRotateTurretCommand,
+                    waitForShooterWheelSpeedCommand
+                    // wait for hood
+                )
+                // kicker
+            )
         );
     }
 
