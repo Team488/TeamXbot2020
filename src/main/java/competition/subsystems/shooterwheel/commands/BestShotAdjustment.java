cd579f10dcd512ca78dbe961ccf0f06805e931cc
lil206chris <big206chris@gmail.com>
2020-02-18 20:49:56 -0800

    made the loop

--- src/main/java/competition/subsystems/shooterwheel/commands/BestShotAdjustment.java
+++ src/main/java/competition/subsystems/shooterwheel/commands/BestShotAdjustment.java
@@ -19,14 +19,16 @@ public class BestShotAdjustment extends BaseCommand{
     final DoubleProperty maxAngle;
     final DoubleProperty maxVelosityProperty;
     final DoubleProperty heightToGoal;
+    final DoubleProperty incrementShotAdjustment;
     public double tvelosity;
 
 
     @Inject
-    public BestShotAdjustment( PropertyFactory pf,  ShooterWheelSubsystem wheel, PoseSubsystem pose, TurretSubsystem turret){
+    public BestShotAdjustment( final PropertyFactory pf,  final ShooterWheelSubsystem wheel, final PoseSubsystem pose, final TurretSubsystem turret){
         this.wheel = wheel;
         this.pose = pose;
         this.turret = turret;
+        incrementShotAdjustment = pf.createPersistentProperty("Increment Shot Adjustment", 400);
         minAngle = pf.createPersistentProperty("MinAngle", 0);
         maxAngle = pf.createPersistentProperty("MaxAngle", 60);
         maxVelosityProperty = pf.createPersistentProperty("MaxVelosity", 20);
@@ -41,29 +43,31 @@ public class BestShotAdjustment extends BaseCommand{
     }
 
     public void execute(){
-        double distance = 10; //TODO: implement vision to find distance away from the goal
+        final double distance = 10; //TODO: implement vision to find distance away from the goal
         if(angleIsPossible(findAngleWVelosity(tvelosity, distance), distance)){
             wheel.setPidSetpoint(tvelosity);
         }
         else{
-            tvelosity+=200;
+            tvelosity+= incrementShotAdjustment.get();
         }
     }
 
-    public double findAngleWVelosity(double velosity, double distance)
+    public double findAngleWVelosity(final double velosity, final double distance)
     {
         return 0.5*Math.asin((32*distance)/(velosity * velosity));
     }
 
-    public double findVelosityWAngle(double theta, double distance, double initHeight){
+    public double findVelosityWAngle(final double theta, final double distance, final double initHeight){
         return Math.sqrt(1/(Math.pow(Math.cos(theta), 2)*(initHeight + (distance * Math.tan(theta)-heightToGoal.get()))));
     }
 
-    public boolean angleIsPossible(double angle, double distance)
+    public boolean angleIsPossible(final double angle, final double distance)
     {
-        double heightOfGoal = 8;
-        double heightAtPeak = distance * Math.tan(angle) - (32)*(distance*distance)*(1/((maxVelosityProperty.get()*maxVelosityProperty.get())));
+        final double heightOfGoal = heightToGoal.get();
+        final double heightAtPeak = distance * Math.tan(angle) - (32)*(distance*distance)*(1/((maxVelosityProperty.get()*maxVelosityProperty.get())));
 
         return (heightOfGoal-heightAtPeak) < -.3;
     }
+
+
 }
