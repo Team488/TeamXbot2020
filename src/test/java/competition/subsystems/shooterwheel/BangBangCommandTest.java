3d433893028df3468f4747105d5b171c0b583e25
Zilong <Zilong3029@gmail.com>
2020-02-18 20:23:39 -0800

    Fixed tests

--- src/test/java/competition/subsystems/shooterwheel/BangBangCommandTest.java
+++ src/test/java/competition/subsystems/shooterwheel/BangBangCommandTest.java
@@ -8,24 +8,33 @@ import competition.BaseCompetitionTest;
 import competition.subsystems.shooterwheel.commands.BangBangCommand;
 
 public class BangBangCommandTest extends BaseCompetitionTest {
+
+    ShooterWheelSubsystem shooterWheelSubsystem;
+    BangBangCommand command;
+    
+    @Override
+    public void setUp() {
+        super.setUp();
+        shooterWheelSubsystem = this.injector.getInstance(ShooterWheelSubsystem.class);
+        command = this.injector.getInstance(BangBangCommand.class);    
+    }
+    
     @Test
     public void testFullPower(){
-        ShooterWheelSubsystem shooterWheelSubsystem = this.injector.getInstance(ShooterWheelSubsystem.class);
-        BangBangCommand command = this.injector.getInstance(BangBangCommand.class);
         shooterWheelSubsystem.setTargetRPM(400);
         command.initialize();
         command.execute();
 
-        assertEquals(1, shooterWheelSubsystem.getPower(), 0.001);
+        assertEquals(0.5, shooterWheelSubsystem.getPower(), 0.001);
     }
     @Test
     public void testZeroPower(){
-        ShooterWheelSubsystem shooterWheelSubsystem = this.injector.getInstance(ShooterWheelSubsystem.class);
-        BangBangCommand command = this.injector.getInstance(BangBangCommand.class);
-        shooterWheelSubsystem.setTargetRPM(-100);
+        shooterWheelSubsystem.setTargetRPM(400);
         command.initialize();
         command.execute();
-
+        assertEquals(0.5, shooterWheelSubsystem.getPower(),0.001);
+        shooterWheelSubsystem.setTargetRPM(0);
+        command.execute();
         assertEquals(0, shooterWheelSubsystem.getPower(), 0.001);
     }
 }
