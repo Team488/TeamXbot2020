5ed466ae8134a3bbdab198f3fa447dc2297376fb
lil206chris <big206chris@gmail.com>
2020-02-18 20:50:42 -0800

    test in the making

---
+++ src/test/java/competition/subsystems/shooterwheel/ShotAdjustmentTest.java
@@ -0,0 +1,13 @@
+package competition.subsystems.shooterwheel;
+
+import competition.BaseCompetitionTest;
+
+public class ShotAdjustmentTest extends BaseCompetitionTest {
+    
+    @Test
+    public void testNumbers() {
+        
+    
+        assertEquals();
+    }
+}
