42fb6a5cbae64a05fc8489d48dfec81e3cd2512c
Stephen Just <stjust@microsoft.com>
2020-02-20 20:15:52 -0800

    Refactor/Pass command should use BaseWaitForMaintainerCommand

--- src/main/java/competition/subsystems/shooterwheel/commands/ShooterWheelWaitForGoalCommand.java
+++ src/main/java/competition/subsystems/shooterwheel/commands/ShooterWheelWaitForGoalCommand.java
@@ -4,40 +4,16 @@ import com.google.inject.Inject;
 
 import competition.subsystems.shooterwheel.ShooterWheelSubsystem;
 import xbot.common.command.BaseCommand;
+import xbot.common.command.BaseWaitForMaintainerCommand;
 import xbot.common.controls.sensors.XTimer;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
 
-public class ShooterWheelWaitForGoalCommand extends BaseCommand {
-
-    private final ShooterWheelSubsystem shooterWheelSubsystem;
-    private final DoubleProperty timeoutProperty;
-
-    private double startTime = -1;
+public class ShooterWheelWaitForGoalCommand extends BaseWaitForMaintainerCommand {
 
     @Inject
-    public ShooterWheelWaitForGoalCommand(PropertyFactory pf, ShooterWheelSubsystem subsystem) {
-        this.shooterWheelSubsystem = subsystem;
-
-        pf.setPrefix(this);
-
-        this.timeoutProperty = pf.createPersistentProperty("Timeout", 1);
-    }
-
-    @Override
-    public void initialize() {
-        log.info("Initializing");
-        this.startTime = XTimer.getFPGATimestamp();
-    }
-
-    @Override
-    public void execute() {
-        // This command doesn't do anything
-    }
-
-    @Override
-    public boolean isFinished() {
-        return this.shooterWheelSubsystem.isMaintainerAtGoal() || XTimer.getFPGATimestamp() - this.startTime > this.timeoutProperty.get();
+    public ShooterWheelWaitForGoalCommand(ShooterWheelSubsystem subsystem, PropertyFactory pf) {
+        super(subsystem, pf, 1);
     }
     
 }
