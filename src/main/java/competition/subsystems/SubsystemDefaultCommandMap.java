79e99f6469dee6a3a6d527b29a41cbd4828b200d
John Gilbert <johngilb@gmail.com>
2020-02-23 13:33:22 -0800

    Hanging system upgrades

--- src/main/java/competition/subsystems/SubsystemDefaultCommandMap.java
+++ src/main/java/competition/subsystems/SubsystemDefaultCommandMap.java
@@ -4,16 +4,15 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import competition.subsystems.arm.ArmSubsystem;
-import competition.subsystems.intake.IntakeSubsystem;
 import competition.subsystems.arm.commands.RaiseArmCommand;
-import competition.subsystems.intake.commands.StopIntakeCommand;
 import competition.subsystems.carousel.CarouselSubsystem;
 import competition.subsystems.carousel.commands.CarouselViaJoystickCommand;
-import competition.subsystems.carousel.commands.StopCarouselCommand;
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.drive.commands.ArcadeDriveCommand;
 import competition.subsystems.hood.HoodSubsystem;
 import competition.subsystems.hood.commands.StopHoodCommand;
+import competition.subsystems.intake.IntakeSubsystem;
+import competition.subsystems.intake.commands.StopIntakeCommand;
 import competition.subsystems.internalconveyor.KickerSubsystem;
 import competition.subsystems.internalconveyor.commands.KickerViaTriggerCommand;
 import competition.subsystems.shooterwheel.ShooterWheelSubsystem;
--- src/main/java/competition/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/competition/subsystems/climber/ClimberSubsystem.java
@@ -2,71 +2,124 @@ package competition.subsystems.climber;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.revrobotics.ControlType;
 
 import competition.IdealElectricalContract;
 import xbot.common.command.BaseSubsystem;
-import xbot.common.controls.actuators.XCANTalon;
 import xbot.common.controls.actuators.XCANSparkMax;
 import xbot.common.injection.wpi_factories.CommonLibFactory;
+import xbot.common.math.MathUtils;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
- 
+
 @Singleton
 public class ClimberSubsystem extends BaseSubsystem {
 
-    double power;
+    public enum ClimberSide {
+        Left, Right
+    }
+
+    double offest;
     final DoubleProperty climberPowerProp;
-    final DoubleProperty extendClimberHeightProp;
-    final DoubleProperty retractClimberHeightProp;
-    public XCANTalon climberMotor;
-    public XCANSparkMax leader;
-    private XCANSparkMax follower;
+    final DoubleProperty maxClimberTicksProp;
+    public XCANSparkMax leftMotor;
+    public XCANSparkMax rightMotor;
     final IdealElectricalContract contract;
+    final DoubleProperty slowZoneProp;
+    final DoubleProperty slowZoneFactorProp;
+    final DoubleProperty defaultPowerProp;
+    final DoubleProperty unsafeExtensionProp;
 
     @Inject
     public ClimberSubsystem(CommonLibFactory factory, PropertyFactory pf, IdealElectricalContract contract) {
         log.info("Creating ClimberSubsystem");
         pf.setPrefix(this);
         climberPowerProp = pf.createPersistentProperty("Climber Power", 1);
-        extendClimberHeightProp = pf.createPersistentProperty("Extend Climber Height", 1);    
-        retractClimberHeightProp = pf.createPersistentProperty("Retract Climber Height", -1); 
+        maxClimberTicksProp = pf.createPersistentProperty("Extend Climber Height", 1);
+        slowZoneProp = pf.createPersistentProperty("Slow Zone Size", 10);
+        slowZoneFactorProp = pf.createPersistentProperty("Slow Zone Factor", 0.35);
+        defaultPowerProp = pf.createPersistentProperty("Default Power", 0.5);
+        unsafeExtensionProp = pf.createPersistentProperty("Unsafe Extension Distance", 3);
+
 
         this.contract = contract;
 
         if (contract.isClimberReady()) {
-            this.leader = factory.createCANSparkMax(contract.climberMotorMaster().channel, this.getPrefix(), "ClimberMaster");
-            this.follower = factory.createCANSparkMax(contract.climberMotorFollower().channel, this.getPrefix(), "ClimberFollower");
-            follower.follow(leader, true);
+            this.leftMotor = factory.createCANSparkMax(contract.leftClimberMotor().channel, this.getPrefix(),
+                    "LeftMotor");
+            this.rightMotor = factory.createCANSparkMax(contract.rightClimberMotor().channel, this.getPrefix(),
+                    "RightMotor");
         }
     }
 
-    public void extendClimber () {
-        setPower(extendClimberHeightProp.get());
+    private XCANSparkMax getMotorForSide(ClimberSide side) {
+        if (side == ClimberSide.Left) {
+            return leftMotor;
+        }
+        return rightMotor;
     }
 
-    public void retractClimber () {
-        setPower(retractClimberHeightProp.get());
+    public double getPosition(ClimberSide side) {
+        if (contract.isClimberReady()) {
+            return getMotorForSide(side).getPosition();
+        }
+        return 0;
     }
 
-    public void grabClamp () {
-        setPower(climberPowerProp.get());
-    }
-
-    public void releaseClamp () {
-        setPower(-1);
-    }
-
-    public void setPower (double power) {
-        if(contract.isClimberReady()) {
-            leader.set(power);
+    public void setPositionalGoal(double positionGoal, ClimberSide side) {
+        if (contract.isClimberReady()) {
+            getMotorForSide(side).setReference(positionGoal, ControlType.kPosition);
         }
     }
 
-    public double getPower() {
-        return leader.get();
+    private void setRawPower(double power, ClimberSide side) {
+        if (contract.isClimberReady()) {
+            getMotorForSide(side).set(power);
+        }
     }
 
-    public void stop () {
-        setPower(0);
+    public void setPower(double power, ClimberSide side) {
+        if (getPosition(side) < 0) {
+            power = MathUtils.constrainDouble(power, 0, 1);
+        }
+
+        if (getPosition(side) < slowZoneProp.get()) {
+            power *= slowZoneFactorProp.get();
+        }
+
+        setRawPower(power, side);
+    }
+
+    public void extend() {
+        setPower(defaultPowerProp.get(), ClimberSide.Left);
+        setPower(defaultPowerProp.get(), ClimberSide.Right);
+    }
+
+    public void retract() {
+        setPower(-defaultPowerProp.get(), ClimberSide.Left);
+        setPower(-defaultPowerProp.get(), ClimberSide.Right);
+    }
+
+    public void stop() {
+        setPower(0, ClimberSide.Left);
+        setPower(0, ClimberSide.Right);
+    }
+
+    public boolean unsafeToLowerArm() {
+        return 
+            getPosition(ClimberSide.Left) > unsafeExtensionProp.get() ||
+            getPosition(ClimberSide.Right) > unsafeExtensionProp.get();
+    }
+
+    public double getSlowZoneRange() {
+        return slowZoneProp.get();
+    }
+
+    public double getSlowZoneFactor() {
+        return slowZoneFactorProp.get();
+    }
+
+    public double getDefaultPower() {
+        return defaultPowerProp.get();
     }
 }
