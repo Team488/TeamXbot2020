39794836e1ed7b4465da1c6d02b11f00db2bf1ff
John Gilbert <johngilb@gmail.com>
2020-03-01 11:05:14 -0800

    add laser

--- src/main/java/competition/multisubsystemcommands/SetWheelAndHoodGoalsCommand.java
+++ src/main/java/competition/multisubsystemcommands/SetWheelAndHoodGoalsCommand.java
@@ -17,7 +17,7 @@ public class SetWheelAndHoodGoalsCommand extends BaseCommand {
     private double hoodPercentGoal;
 
     public enum FieldPosition {
-        PointBlank, InitiationCloseToGoal, TrenchCloseToGoal, TrenchFarFromGoal, InitiationFarFromGoal
+        PointBlank, InitiationLaser, InitiationCloseToGoal, TrenchCloseToGoal, TrenchFarFromGoal, InitiationFarFromGoal
     }
 
     private final DoubleProperty pointBlankRpmProp;
@@ -25,6 +25,9 @@ public class SetWheelAndHoodGoalsCommand extends BaseCommand {
 
     private final DoubleProperty initiationCloseToGoalRpmProp;
     private final DoubleProperty initiationClosetoGoalHoodProp;
+
+    private final DoubleProperty initiationLaserRpmProp;
+    private final DoubleProperty initiationLaserHoodProp;
 
     private final DoubleProperty trenchCloseToGoalRpmProp;
     private final DoubleProperty trenchCloseToGoalHoodProp;
@@ -48,6 +51,10 @@ public class SetWheelAndHoodGoalsCommand extends BaseCommand {
         // Lob shot
         initiationCloseToGoalRpmProp = pf.createPersistentProperty("Initiation CLOSE to Goal RPM", 2400);
         initiationClosetoGoalHoodProp = pf.createPersistentProperty("Initiation CLOSE to Goal Hood", 0.1);
+
+        // Laser for autonomous
+        initiationLaserRpmProp = pf.createPersistentProperty("Initiation LASER Goal RPM", 2400);
+        initiationLaserHoodProp = pf.createPersistentProperty("Initiation LASER Goal Hood", 0.1);
 
         // Tested, works
         trenchCloseToGoalRpmProp = pf.createPersistentProperty("Trench CLOSE to Goal RPM", 3300);
@@ -77,6 +84,8 @@ public class SetWheelAndHoodGoalsCommand extends BaseCommand {
         case InitiationCloseToGoal:
             setGoals(initiationCloseToGoalRpmProp.get(), initiationClosetoGoalHoodProp.get());
             break;
+            case InitiationLaser:
+            setGoals(initiationLaserRpmProp.get(), initiationLaserHoodProp.get());
         case TrenchCloseToGoal:
             setGoals(trenchCloseToGoalRpmProp.get(), trenchCloseToGoalHoodProp.get());
             break;
