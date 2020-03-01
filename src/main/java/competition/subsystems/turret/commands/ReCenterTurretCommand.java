9e5d3197347ab513ea9e8cb4c012b5351aba5d5e
ivanthich <ivanthich2@gmail.com>
2020-03-01 10:54:02 -0800

    Made ReCenterTurretCommand to readjust turret

---
+++ src/main/java/competition/subsystems/turret/commands/ReCenterTurretCommand.java
@@ -0,0 +1,29 @@
+package competition.subsystems.turret.commands;
+
+import com.google.inject.Inject;
+
+import competition.subsystems.turret.TurretSubsystem;
+import xbot.common.command.BaseSetpointCommand;
+
+public class ReCenterTurretCommand extends BaseSetpointCommand{
+
+    final TurretSubsystem turret;
+    private double headingGoal = -90; // recenter value, can also be 270
+
+    @Inject
+    public ReCenterTurretCommand(TurretSubsystem turret){
+        super(turret);
+        this.turret = turret;
+    }
+
+    @Override
+    public void initialize(){
+        log.info("Initializing");
+        turret.setFieldOrientedGoalAngle(headingGoal);
+    }
+
+    @Override
+    public void execute(){
+    }
+
+}
