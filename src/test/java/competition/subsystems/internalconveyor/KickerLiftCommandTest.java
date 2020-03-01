c4369fc0dfada05a5e9b8635111f83bbd0e5ba26
ivanthich <ivanthich2@gmail.com>
2020-02-29 17:26:26 -0800

    Created KickerLift and KickerReverse tests

---
+++ src/test/java/competition/subsystems/internalconveyor/KickerLiftCommandTest.java
@@ -0,0 +1,32 @@
+package competition.subsystems.internalconveyor;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import competition.BaseCompetitionTest;
+import competition.subsystems.internalconveyor.commands.KickerLiftCommand;
+
+public class KickerLiftCommandTest extends BaseCompetitionTest{
+
+    @Test
+    public void kickerLiftWheel(){
+        KickerLiftCommand lift = this.injector.getInstance(KickerLiftCommand.class);
+        KickerSubsystem kicker = this.injector.getInstance(KickerSubsystem.class);
+
+        lift.execute();
+
+        assertEquals(1 , kicker.wheelMotor.getMotorOutputPercent(), 0.001);
+    }
+
+    @Test
+    public void kickerLiftRoller(){
+        KickerSubsystem kicker = this.injector.getInstance(KickerSubsystem.class);
+        KickerLiftCommand lift = this.injector.getInstance(KickerLiftCommand.class);
+
+        lift.execute();
+
+        assertEquals(-1, kicker.rollerMotor.getMotorOutputPercent(), 0.001);
+    }
+
+}
---
+++ src/test/java/competition/subsystems/internalconveyor/KickerReverseCommandTest.java
@@ -0,0 +1,31 @@
+package competition.subsystems.internalconveyor;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import competition.BaseCompetitionTest;
+import competition.subsystems.internalconveyor.commands.KickerReverseCommand;
+
+public class KickerReverseCommandTest extends BaseCompetitionTest{
+
+    @Test
+    public void kickerReverseWheel(){
+        KickerReverseCommand reverse = this.injector.getInstance(KickerReverseCommand.class);
+        KickerSubsystem kicker = this.injector.getInstance(KickerSubsystem.class);
+
+        reverse.execute();
+
+        assertEquals(-0.25 , kicker.wheelMotor.getMotorOutputPercent(), 0.001);
+    }
+
+    @Test
+    public void kickerReverseRoller(){
+        KickerSubsystem kicker = this.injector.getInstance(KickerSubsystem.class);
+        KickerReverseCommand reverse = this.injector.getInstance(KickerReverseCommand.class);
+
+        reverse.execute();
+
+        assertEquals(-0.25, kicker.rollerMotor.getMotorOutputPercent(), 0.001);
+    }
+}
