5a8d491f6122a486cfa110d7cad638842fb48129
ivanthich <ivanthich2@gmail.com>
2020-03-01 16:15:50 -0800

    Original name doesn't match with the function of this test, so renamed it to accurately match the tests' function

--- src/test/java/competition/subsystems/vision/SendXYThetaPosTest.java
+++ src/test/java/competition/subsystems/vision/SendXYThetaPosTest.java
@@ -4,11 +4,10 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import competition.BaseCompetitionTest;
 import competition.subsystems.pose.PoseSubsystem;
 import competition.subsystems.vision.commands.SendInitialPositionToRaspberryCommand;
 
-public class SendInitialPositionToRaspberryCommandTest extends BaseCompetitionTest{
+public class SendXYThetaPosTest extends VisionSubsystemTest{
 
     SendInitialPositionToRaspberryCommand initial;
     VisionSubsystem vision;
