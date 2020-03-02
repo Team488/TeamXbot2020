7fc64c92f54fe18806899bd64f871b20fcfee810
John Gilbert <johngilb@gmail.com>
2020-03-01 18:27:00 -0800

    Also include autonomous command

--- src/main/java/competition/commandgroups/BasicAutonomousCommand.java
+++ src/main/java/competition/commandgroups/BasicAutonomousCommand.java
@@ -3,7 +3,6 @@ package competition.commandgroups;
 import com.google.inject.Inject;
 
 import competition.multisubsystemcommands.SetWheelAndHoodGoalsCommand;
-import competition.subsystems.carousel.commands.CarouselFiringModeCommand;
 import competition.subsystems.drive.commands.AutonomousDriveCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
@@ -20,8 +19,7 @@ public class BasicAutonomousCommand extends SequentialCommandGroup {
     public BasicAutonomousCommand(
         PropertyFactory pf,
         SetWheelAndHoodGoalsCommand setGoals,
-        PrepareToFireCommand prepare,
-        CarouselFiringModeCommand spinCarousel,
+        ShootCommand shoot,
         ShutdownShootingCommand stopShooting,
         AutonomousDriveCommand drive)
     {
@@ -36,7 +34,7 @@ public class BasicAutonomousCommand extends SequentialCommandGroup {
         var timeToDrive = new DelayViaSupplierCommand(() -> 1.0);
         drive.setDrivePower(0.5, 0.5);
 
-        var shootSequence = new ParallelCommandGroup(setGoals, prepare, spinCarousel);
+        var shootSequence = new ParallelCommandGroup(setGoals, shoot);
         var driveSequence = new ParallelCommandGroup(stopShooting, drive);
         
         var shootForTime = new ParallelDeadlineGroup(timeToShoot, shootSequence);
