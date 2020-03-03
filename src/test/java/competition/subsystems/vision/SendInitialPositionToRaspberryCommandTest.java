7dd97824dd1fefeeae3dbb1cdfd78ae32007ea8d
ivanthich <ivanthich2@gmail.com>
2020-03-03 09:05:22 -0800

    Created new SendInitialPositionToRaspberryCommandTest template

---
+++ src/test/java/competition/subsystems/vision/SendInitialPositionToRaspberryCommandTest.java
@@ -0,0 +1,15 @@
+package competition.subsystems.vision;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import competition.BaseCompetitionTest;
+import competition.subsystems.pose.PoseSubsystem;
+import competition.subsystems.vision.commands.SendInitialPositionToRaspberryCommand;
+
+public class SendInitialPositionToRaspberryCommandTest extends BaseCompetitionTest{
+    SendInitialPositionToRaspberryCommand initial;
+
+
+}
